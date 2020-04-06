929ba6d03e57858ae321de01d0004ffeae6ad974
Ian <ian.leedham@ehsdata.com>
2020-04-06 16:59:05 +0100

    created card manager and I card manager so they can be injected

---
+++ Api/Interfaces/ICardManager.cs
@@ -0,0 +1,19 @@
+﻿using FirstCateringAuthenticationApi.Classes;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FirstCateringAuthenticationApi.Interfaces
+{
+    public interface ICardManager
+    {
+        Task<IdentityResult> CreateAsync(IdentityCard card);
+
+        Task<IdentityResult> CreateAsync(IdentityCard card, string password);
+
+        Task<IdentityCard> FindByIdAsync(string userId);
+
+        Task<IdentityCard> GetUserAsync(ClaimsPrincipal principal);
+
+    }
+}
---
+++ Api/Managers/CardManager.cs
@@ -0,0 +1,17 @@
+﻿using FirstCateringAuthenticationApi.Classes;
+using FirstCateringAuthenticationApi.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+
+namespace FirstCateringAuthenticationApi.Managers
+{
+    public class CardManager : UserManager<IdentityCard>, ICardManager
+    {
+        public CardManager(IUserStore<IdentityCard> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<IdentityCard> passwordHasher, IEnumerable<IUserValidator<IdentityCard>> userValidators, IEnumerable<IPasswordValidator<IdentityCard>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<IdentityCard>> logger) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        {
+        }
+    }
+}
