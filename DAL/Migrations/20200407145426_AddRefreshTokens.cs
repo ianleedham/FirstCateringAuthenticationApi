76c38103328ec0d2fd1e449fde87726469e1dafd
Ian <ian.leedham@ehsdata.com>
2020-04-07 16:41:32 +0100

    added refresher tokens to database and migrations

---
+++ DAL/Migrations/20200407145426_AddRefreshTokens.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AddRefreshTokens : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Token = table.Column<string>(nullable: false),
+                    CardNumber = table.Column<string>(nullable: true),
+                    Revoked = table.Column<bool>(nullable: false),
+                    Expires = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Token);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+        }
+    }
+}
