8f5630c6c03b8ac500af19397d71b8f9e69109c3
Ian <ian.leedham@ehsdata.com>
2020-04-07 16:41:55 +0100

    added to auto mapper profile

--- Api/Mappings/AutoMapperProfile.cs
+++ Api/Mappings/AutoMapperProfile.cs
@@ -1,7 +1,25 @@
-﻿namespace FirstCateringAuthenticationApi.Mappings
+﻿using AutoMapper;
+using DAL.Models;
+using FirstCateringAuthenticationApi.DataTransferObjects;
+
+namespace FirstCateringAuthenticationApi.Mappings
 {
-    public class AutoMapperProfile
+    public class AutoMapperProfile: Profile
     {
-        
+        public AutoMapperProfile()
+        {
+            CreateMap<CardRegistrationDto, IdentityCard>()
+                .ForMember(dest => dest.FirstName, o => o.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, o => o.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.Id, o => o.MapFrom(src => src.CardNumber))
+                .ForMember(dest => dest.UserName, o => o.MapFrom(src => src.EmployeeId))
+                .ForMember(dest => dest.Email, o => o.MapFrom(src => src.Email))
+                .ForMember(dest => dest.PhoneNumber, o => o.MapFrom(src => src.PhoneNumber));
+
+            CreateMap<IdentityCard, CardDto>()
+                .ForMember(dest => dest.RefreshToken, o => o.Ignore())
+                .ForMember(dest => dest.Bearer, o => o.Ignore())
+                .ForMember(dest => dest.CardNumber, o => o.MapFrom(dest => dest.Id));
+        }
     }
 }
