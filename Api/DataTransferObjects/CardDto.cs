711b3c6c4c5685ca76b4a447756b1b3ac01e6f21
Ian <ian.leedham@ehsdata.com>
2020-04-07 14:54:21 +0100

    add dtos

---
+++ Api/DataTransferObjects/CardDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstCateringAuthenticationApi.DataTransferObjects
+{
+    public class CardDto
+    {
+        public string CardNumber { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bearer { get; set; }
+        
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ Api/DataTransferObjects/CardRegistrationDto.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstCateringAuthenticationApi.DataTransferObjects
+{
+    public class CardRegistrationDto
+    {
+        public string CardNumber { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumer { get; set; }
+
+        public string Pin { get; set; }
+
+    }
+}
