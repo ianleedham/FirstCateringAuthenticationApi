15cc9828b39fcfe5d8d459ae999071d92fd71368
Ian <ian.leedham@ehsdata.com>
2020-04-07 14:54:07 +0100

    add jwt, cors and injectables to startup

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,12 +1,17 @@
-using FirstCateringAuthenticationApi.Classes;
+using DAL;
+using DAL.Interfaces;
+using DAL.Models;
+using DAL.Repositories;
 using FirstCateringAuthenticationApi.Interfaces;
 using FirstCateringAuthenticationApi.Managers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FirstCateringAuthenticationApi
 {
@@ -22,7 +27,10 @@ namespace FirstCateringAuthenticationApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AuthenticationContext>(o => o.UseSqlServer("Server=IANSPC02\\MSSQLSERVER01;Database=FirstCateringAuthentication;Trusted_Connection=True;"));
+            services.AddCors(x => x.AddDefaultPolicy(builder => builder.AllowAnyOrigin().Build()));
+            services.AddControllers();
+            services.AddDbContext<AuthenticationContext>(o => o.
+                UseSqlServer("Server=IANSPC02\\MSSQLSERVER01;Database=FirstCateringAuthentication;Trusted_Connection=True;"));
 
             services.AddIdentityCore<IdentityCard>(o =>
             {
@@ -35,14 +43,33 @@ namespace FirstCateringAuthenticationApi
             })
                 .AddEntityFrameworkStores<AuthenticationContext>();
 
-            services.AddTransient<ICardManager, CardManager>();
-            // services.AddTransient<ICardSignInManager, CardSignInManager>();
-            services.AddControllers();
+            services.AddHttpContextAccessor();
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = true;
+                    x.SaveToken = true;
+                    x.IncludeErrorDetails = true;
+                    x.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false,
+                        ValidateLifetime = true,
+                    };
+                });
+
+            services.AddScoped<ICardManager, CardManager>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -52,8 +79,8 @@ namespace FirstCateringAuthenticationApi
 
             app.UseRouting();
 
-            // app.UseAuthentication();
-            // app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
