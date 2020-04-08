0bb91f13d303f7d3a5cdba9a0cdef7913798cb53
Ian <ian.leedham@ehsdata.com>
2020-04-08 11:34:25 +0100

    populated CardRegistrationBuilder

--- UnitTests/Builders/CardRegistrationDtoBuilder.cs
+++ UnitTests/Builders/CardRegistrationDtoBuilder.cs
@@ -4,15 +4,38 @@ namespace UnitTests.Builders
 {
     public class CardRegistrationDtoBuilder
     {
+        private string _cardNumber;
+        private string _employeeId;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _phoneNumber;
+        private string _pin;
+
         public CardRegistrationDtoBuilder GenericRegistration()
         {
-            
+            _cardNumber = "1234hgte5678inngs";
+            _employeeId = "123456";
+            _firstName = "Ian";
+            _lastName = "Leedham";
+            _email = "ian.leedham@ehsdata.com";
+            _phoneNumber = "+44 7954865812";
+            _pin = "1234";
             return this;
         }
 
         public CardRegistrationDto Build()
         {
-            return new CardRegistrationDto();
+            return new CardRegistrationDto()
+            {
+                CardNumber = _cardNumber,
+                EmployeeId = _employeeId,
+                FirstName = _firstName,
+                LastName = _lastName,
+                Email = _email,
+                PhoneNumber = _phoneNumber,
+                Pin = _pin
+            };
         }
     }
 }
