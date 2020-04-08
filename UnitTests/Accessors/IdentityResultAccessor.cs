bc5e799329cc0a6894b35a6820def65373bf7c15
Ian <ian.leedham@ehsdata.com>
2020-04-08 15:10:19 +0100

    added and fixed unit tests

---
+++ UnitTests/Accessors/IdentityResultAccessor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UnitTests.Accessors
+{
+    public class IdentityResultAccessor : IdentityResult
+    {
+        
+        public IdentityResult Value { get; }
+        
+        public IdentityResultAccessor(bool succeeded)
+        {
+            if (succeeded)
+            {
+                Value = new IdentityResult();
+                {
+                    Succeeded = true;
+                }
+            }
+        }
+    }
+}
--- UnitTests/AuthenticationControllerTests.cs
+++ UnitTests/AuthenticationControllerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using FirstCateringAuthenticationApi.Controllers;
 using FirstCateringAuthenticationApi.Interfaces;
 using Microsoft.AspNetCore.Identity;
@@ -8,7 +9,9 @@ using AutoMapper;
 using DAL.Interfaces;
 using DAL.Models;
 using FirstCateringAuthenticationApi.DataTransferObjects;
+using FirstCateringAuthenticationApi.Model;
 using Microsoft.Extensions.Configuration;
+using UnitTests.Accessors;
 using UnitTests.Builders;
 using Xunit;
 
@@ -31,46 +34,65 @@ namespace UnitTests
         }
 
         [Fact]
-        public async Task UserTaps_WithLoggedinUser_SetsRefreshTokenToRevokedReturnsOk()
+        public async Task UserTaps_WithLoggedinUser_InvalidatesRefreshTokenReturnsOk()
         {
             // Arrange
             var subject = GetSubject();
             IdentityCard card = _identityCardBuilder.GenericIdentityCard().Build();
-            card.Id = "cutbgln213454hbv";
             _mockCardManager.Setup(x => x.FindByIdAsync(card.Id)).ReturnsAsync(card);
-
+            
             // Act
-            var result = await subject.Logout(card.Id);
+            var result = await subject.Tap(card.Id);
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(typeof(OkObjectResult), result.GetType());
-            Assert.Equal(typeof(IdentityCard), (result as BadRequestObjectResult).Value.GetType());
+            Assert.Equal(typeof(OkResult), result.GetType());
+            _mockTokenManager.Verify(x => x.InvalidateRefreshToken(card.Id));
         }
         
         [Fact]
-        public async Task UserTaps_WithUnregisteredCard_ReturnsBadRequest()
+        public async Task UserTaps_WithUnregisteredCard_ReturnsUnauthorised()
+        {
+            // Arrange
+            var subject = GetSubject();
+            IdentityCard card = _identityCardBuilder.GenericIdentityCard().Build();
+            
+            // Act
+            var result = await subject.Tap(card.Id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(typeof(OkResult), result.GetType());
+            _mockTokenManager.Verify(x => x.InvalidateRefreshToken(card.Id));
+        }
+        
+        [Fact]
+        public async Task ClientCallsLogin_WithUnregisteredCard_ReturnsNotfound()
         {
             // Arrange
             var subject = GetSubject();
             var cardNumber = "cutbgln213454hbv";
+            var pin = "1234";
             _mockCardManager.Setup(x => x.FindByIdAsync(cardNumber)).ReturnsAsync((IdentityCard) null);
-
+            var loginParameters = new LoginParametersDto();
+            
             // Act
-            var result = await subject.Logout(cardNumber);
+            var result = await subject.Login(loginParameters);
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(typeof(BadRequestResult), result.GetType());
-            Assert.Equal("Please register your card", (result as BadRequestObjectResult).Value);
+            Assert.Equal(typeof(NotFoundObjectResult), result.GetType());
+            Assert.Equal("please register your card", (result as NotFoundObjectResult).Value);
         }
 
         [Fact]
-        public async Task UserTaps_WithRegisteredCardAndCorrectPin_LogsUserInAndReturnsCardDto()
+        public async Task ClientSendsLoginInfo_WithRegisteredCardAndCorrectPin_LogsUserInAndReturnsCardDto()
         {
             // Arrange
             var subject = GetSubject();
-            var pin = "1234";
+            const string pin = "1234";
+            const string jwt = "mock1jwt";
+            var refreshTokenValue = "mock1refreshToken";
             var card = _identityCardBuilder.GenericIdentityCard().Build();
             card.Id = "cutbgln213454hbv";
             _mockCardManager.Setup(x => x.FindByIdAsync(card.Id)).ReturnsAsync(card);
@@ -82,10 +104,17 @@ namespace UnitTests
             };
             var cardDto = new CardDto()
             {
-                FullName = card.FullName
+                CardNumber = card.Id,
+                FullName = card.FullName,
+            };
+            var refreshToken = new RefreshToken()
+            {
+                Token = refreshTokenValue
             };
             _mockMapper.Setup(x => x.Map<CardDto>(card)).Returns(cardDto);
-
+            _mockTokenManager.Setup(x => x.CreateJwt(card.Id)).Returns(jwt);
+            _mockTokenManager.Setup(x => x.CreateRefreshToken(card.Id)).Returns(refreshToken);
+            _mockTokenManager.Setup(x => x.SaveRefreshToken(refreshToken)).ReturnsAsync(true);
             // Act
             var result = await subject.Login(loginParameters);
 
@@ -93,13 +122,16 @@ namespace UnitTests
             Assert.NotNull(result);
             Assert.Equal(typeof(OkObjectResult), result.GetType());
             var okObject = result as OkObjectResult;
-            Assert.Equal(typeof(CardDto), (okObject).Value.GetType());
-            Assert.Equal("Ian Leedham", (okObject.Value as CardDto).FullName);
-            Assert.Equal(card.Id, (okObject.Value as CardDto).CardNumber);
+            Assert.Equal(typeof(CardDto), okObject.Value.GetType());
+            var resultCardDto = (CardDto) okObject.Value;
+            Assert.Equal(card.FullName, resultCardDto.FullName);
+            Assert.Equal(card.Id, resultCardDto.CardNumber);
+            Assert.Equal(jwt, resultCardDto.Bearer);
+            Assert.Equal(refreshTokenValue, resultCardDto.RefreshToken);
         }
         
         [Fact]
-        public async Task UserTaps_WithRegisteredCardAndIncorrectPin_ReturnsUnauthorisedWithMessage()
+        public async Task ClientSendsLoginInfo_WithRegisteredCardAndIncorrectPin_ReturnsUnauthorised()
         {
             // Arrange
             var subject = GetSubject();
@@ -130,15 +162,19 @@ namespace UnitTests
             // Arrange
             var subject = GetSubject();
             IdentityCard identityCard = _identityCardBuilder.GenericIdentityCard().Build();
-            identityCard.Id = "cutbgln213454hbv";
             CardRegistrationDto cardRegistrationDto = _cardRegistrationDtoBuilder.GenericRegistration().Build();
+            identityCard.Id = cardRegistrationDto.CardNumber;
             _mockMapper.Setup(x => x.Map<IdentityCard>(cardRegistrationDto)).Returns(identityCard);
 
-            var identityResult = new IdentityResult();
-
-            _mockCardManager.Setup(x => x.
-                CreateAsync(identityCard, cardRegistrationDto.Pin)).ReturnsAsync(identityResult);
-            _mockCardManager.Setup(x => x.FindByIdAsync(identityCard.Id)).ReturnsAsync(identityCard);
+            var identityResult = new IdentityResultAccessor(true);
+            _mockCardManager.Setup(x => x.CreateAsync(identityCard, cardRegistrationDto.Pin))
+                .ReturnsAsync(identityResult);
+            _mockCardManager.SetupSequence(x => x.FindByIdAsync(identityCard.Id))
+                .ReturnsAsync((IdentityCard) null)
+                .ReturnsAsync(identityCard);
+            _mockCardManager.SetupSequence(x => x.FindByIdAsync(identityCard.Id))
+                .ReturnsAsync((IdentityCard) null)
+                .ReturnsAsync(identityCard);
 
             // Act
             var result = await subject.RegisterAsync(cardRegistrationDto);
@@ -146,7 +182,77 @@ namespace UnitTests
             // Assert
             Assert.NotNull(result);
             Assert.Equal(typeof(OkObjectResult), result.GetType());
-            Assert.Equal("Registered successfully", (result as OkObjectResult).Value);
+            Assert.Equal(typeof(IdentityCard), ((OkObjectResult) result).Value.GetType());
+        }
+
+        [Fact]
+        public async Task ClientRefreshToken_WithExpiredRefreshToken_ReturnsUnauthorized()
+        {
+            // Arrange
+            var subject = GetSubject();
+            const string refreshTokenString = "12345jhhjvhk567rgyuhjkln";
+            var refreshToken = new RefreshToken()
+            {
+                Revoked = false,
+                Expires = DateTime.Now.AddHours(-1)
+            };
+            _mockRefreshTokenRepository.Setup(x => x.Get(refreshTokenString)).ReturnsAsync(refreshToken);
+            
+            // Act
+            
+            var result = await subject.RefreshToken(refreshTokenString);
+
+            // Assert
+            Assert.Equal(typeof(UnauthorizedResult), result.GetType());
+        }
+        
+        [Fact]
+        public async Task ClientRefreshToken_WithRevokedRefreshToken_ReturnsUnauthorized()
+        {
+            // Arrange
+            var subject = GetSubject();
+            const string refreshTokenString = "12345jhhjvhk567rgyuhjkln";
+            var refreshToken = new RefreshToken()
+            {
+                Revoked = true,
+                Expires = DateTime.Now.AddDays(1)
+            };
+            _mockRefreshTokenRepository.Setup(x => x.Get(refreshTokenString)).ReturnsAsync(refreshToken);
+            
+            // Act
+            
+            var result = await subject.RefreshToken(refreshTokenString);
+
+            // Assert
+            Assert.Equal(typeof(UnauthorizedResult), result.GetType());
+        }
+        
+        [Fact]
+        public async Task ClientRefreshToken_WithValidRefreshToken_ReturnsOkWithCardDto()
+        {
+            // Arrange
+            var subject = GetSubject();
+            const string refreshTokenString = "12345jhhjvhk567rgyuhjkln";
+            var identityCard = _identityCardBuilder.GenericIdentityCard().Build();
+            string cardNumber = identityCard.Id;
+            var refreshToken = new RefreshToken()
+            {
+                CardNumber = cardNumber,
+                Revoked = false,
+                Expires = DateTime.Now.AddDays(1)
+            };
+            _mockRefreshTokenRepository.Setup(x => x.Get(refreshTokenString)).ReturnsAsync(refreshToken);
+            CardDto cardDto = new CardDto();
+            _mockMapper.Setup(x => x.Map<CardDto>(identityCard)).Returns(cardDto);
+            _mockCardManager.Setup(x => x.FindByIdAsync(cardNumber)).ReturnsAsync(identityCard);
+            _mockTokenManager.Setup(x => x.SaveRefreshToken(refreshToken)).ReturnsAsync(true);
+            
+            // Act
+            var result = await subject.RefreshToken(refreshTokenString);
+
+            // Assert
+            Assert.Equal(typeof(OkObjectResult), result.GetType());
+            Assert.Equal(typeof(CardDto), ((OkObjectResult)result).Value.GetType());
         }
 
         private AuthenticationController GetSubject()
