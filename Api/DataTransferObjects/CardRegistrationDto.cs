7487389e1a7e642676186691fe4be99b94ccbdc3
Ian <ian.leedham@ehsdata.com>
2020-04-08 12:04:50 +0100

    added validation to CardRegistrationDto.cs

--- Api/DataTransferObjects/CardRegistrationDto.cs
+++ Api/DataTransferObjects/CardRegistrationDto.cs
@@ -1,19 +1,28 @@
-﻿namespace FirstCateringAuthenticationApi.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstCateringAuthenticationApi.DataTransferObjects
 {
     public class CardRegistrationDto
     {
+        [Required]
         public string CardNumber { get; set; }
 
+        [Required]
         public string EmployeeId { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string PhoneNumber { get; set; }
 
+        [Required]
         public string Pin { get; set; }
 
     }
